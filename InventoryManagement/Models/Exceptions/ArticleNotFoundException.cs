16196f8f1d8f0b0b3e5beabe11a733193f991619
Chrisi <>
2024-07-27 18:51:43 +0200

    commented the exception classes

--- InventoryManagement/Models/Exceptions/ArticleNotFoundException.cs
+++ InventoryManagement/Models/Exceptions/ArticleNotFoundException.cs
@@ -1,9 +1,19 @@
 ﻿namespace InventoryManagement.Models.Exceptions
 {
+    /// <summary>
+    /// Represents an error when an article is not available
+    /// </summary>
     public class ArticleNotFoundException : Exception
     {
+        /// <summary>
+        /// Gets the ID of the queried article
+        /// </summary>
         public int Id { get; }
 
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="id">The ID of the queried article</param>
         public ArticleNotFoundException(int id)
         : base($"No Article with ID {id} exists")
         {
--- InventoryManagement/Models/Exceptions/DuplicateArticleException.cs
+++ InventoryManagement/Models/Exceptions/DuplicateArticleException.cs
@@ -2,10 +2,20 @@
 
 namespace InventoryManagement.Models.Exceptions;
 
+/// <summary>
+/// Represents an error when an article already exists
+/// </summary>
 public class DuplicateArticleException : Exception
 {
+    /// <summary>
+    /// Gets the duplicated article
+    /// </summary>
     public Article Article { get; }
 
+    /// <summary>
+    /// Creates a new instance
+    /// </summary>
+    /// <param name="article">The duplicated article</param>
     public DuplicateArticleException(Article article)  
         : base($"An Article with ID {article.Id} already exists")
     {
